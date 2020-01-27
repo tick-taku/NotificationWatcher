526960c523330daf3de81e548710a365b0c471b1
tick.taku <tick.taku.77@gmail.com>
2020-01-27 14:55:31 +0900

    Show confirmation dialog.

--- app/src/main/java/com/tick/taku/notificationwatcher/MainActivity.kt
+++ app/src/main/java/com/tick/taku/notificationwatcher/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import androidx.core.app.NotificationManagerCompat
+import com.tick.taku.notificationwatcher.corecomponent.util.showDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,9 +20,13 @@ class MainActivity : AppCompatActivity() {
      * Check permission for notification listener service is granted.
      */
     private fun checkForPermission() {
-        if (!NotificationManagerCompat.getEnabledListenerPackages(this).contains(packageName)) {
-            // TODO: Show dialog
-            startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
-        }
+        if (!NotificationManagerCompat.getEnabledListenerPackages(this).contains(packageName))
+            showDialog {
+                setMessage(R.string.notification_listener_denied)
+                setNegativeButton(R.string.cancel)
+                setPositiveButton(R.string.ok) {
+                    startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
+                }
+            }
     }
 }
