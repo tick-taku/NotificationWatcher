d8c7cfb38df1fe7b54f89e524dbadd1868f02b32
tick.taku <tick.taku.77@gmail.com>
2020-01-27 11:47:41 +0900

    Add permission check.

--- app/src/main/java/com/tick/taku/notificationwatcher/MainActivity.kt
+++ app/src/main/java/com/tick/taku/notificationwatcher/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.tick.taku.notificationwatcher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkForPermission()
+    }
+
+    /**
+     * Check permission for notification listener service is granted.
+     */
+    private fun checkForPermission() {
+        if (!NotificationManagerCompat.getEnabledListenerPackages(this).contains(packageName)) {
+            // TODO: Show dialog
+            startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
+        }
     }
 }
