f7abdf17ed1e0656a5653a83b0d0c0f1598a17a9
tick.taku <tick.taku.77@gmail.com>
2020-01-27 14:40:42 +0900

    Create common dialog.

---
+++ app/src/main/java/com/tick/taku/notificationwatcher/corecomponent/util/ActivityUtils.kt
@@ -0,0 +1,14 @@
+package com.tick.taku.notificationwatcher.corecomponent.util
+
+import androidx.appcompat.app.AppCompatActivity
+import com.tick.taku.notificationwatcher.view.dialog.ApplicationDialog
+
+/**
+ * Show common dialog.
+ */
+fun AppCompatActivity.showDialog(factory: ApplicationDialog.Factory.() -> Unit) {
+    val dialog = ApplicationDialog.Factory(applicationContext).apply {
+        factory(this)
+    }.create()
+    dialog.show(supportFragmentManager)
+}
