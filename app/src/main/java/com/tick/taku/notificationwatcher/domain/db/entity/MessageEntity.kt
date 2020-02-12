97ebdffbf54355b1d1223519668130a057a51c98
tick.taku <tick.taku.77@gmail.com>
2020-02-12 13:18:25 +0900

    Add relation to room and message.

--- app/src/main/java/com/tick/taku/notificationwatcher/domain/db/entity/MessageEntity.kt
+++ app/src/main/java/com/tick/taku/notificationwatcher/domain/db/entity/MessageEntity.kt
@@ -2,10 +2,19 @@ package com.tick.taku.notificationwatcher.domain.db.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
 
-@Entity(tableName = "message")
-data class MessageEntity(@PrimaryKey @ColumnInfo(name = "message_id") val id: String,
+@Entity(tableName = "message",
+        primaryKeys = ["message_id", "room_id"],
+        foreignKeys = [
+            ForeignKey(
+                entity = RoomEntity::class,
+                parentColumns = [RoomEntity.PRIMARY_KEY],
+                childColumns = ["room_id"],
+                onDelete = ForeignKey.CASCADE
+            )
+        ])
+data class MessageEntity(@ColumnInfo(name = "message_id") val id: String,
+                         @ColumnInfo(name = "room_id") val roomId: String,
                          val date: String,
-                         val user: String,
                          val message: String)
--- app/src/main/java/com/tick/taku/notificationwatcher/domain/db/entity/RoomEntity.kt
+++ app/src/main/java/com/tick/taku/notificationwatcher/domain/db/entity/RoomEntity.kt
@@ -5,7 +5,11 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "room")
-data class RoomEntity(@PrimaryKey @ColumnInfo(name = "room_id") val id: String,
+data class RoomEntity(@PrimaryKey @ColumnInfo(name = PRIMARY_KEY) val id: String,
                       val user: String,
                       @ColumnInfo(name = "latest_message") val latestMessage: String,
-                      @ColumnInfo(name = "latest_update") val latestUpdate: String)
+                      @ColumnInfo(name = "latest_update") val latestUpdate: String) {
+    companion object {
+        const val PRIMARY_KEY = "room_id"
+    }
+}
