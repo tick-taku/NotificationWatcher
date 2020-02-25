625739a55ecc4d13c5d0203cdd79659646ea78d9
tick.taku <tick.taku.77@gmail.com>
2020-02-25 18:04:48 +0900

    Fix to crash when move to background during dialog showing.

--- corecomponent/src/main/java/com/tick/taku/android/corecomponent/ui/ApplicationDialog.kt
+++ corecomponent/src/main/java/com/tick/taku/android/corecomponent/ui/ApplicationDialog.kt
@@ -4,80 +4,80 @@ import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
-import android.os.Parcelable
 import android.view.View
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
-import kotlinx.android.parcel.Parcelize
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 
-class ApplicationDialog: DialogFragment() {
+class ApplicationDialog internal constructor(): DialogFragment() {
+
+    //------------------------------------------------------------------------------------------------------------------
+    // MARK: ViewModel
+    //------------------------------------------------------------------------------------------------------------------
+
+    internal data class ApplicationDialogModel(
+        var priority: Priority = Priority.LOW,
+        var isCancelable: Boolean = true,
+        var title: String? = null,
+        var message: String? = null,
+        var positiveButton: Pair<String, DialogInterface.OnClickListener>? = null,
+        var negativeButton: Pair<String, DialogInterface.OnClickListener>? = null,
+        var customView: View? = null
+    ): ViewModel()
 
     //------------------------------------------------------------------------------------------------------------------
     // MARK: Factory
     //------------------------------------------------------------------------------------------------------------------
 
-    @Parcelize
-    data class Model(var priority: Priority = Priority.LOW,
-                     var isCancelable: Boolean = true,
-                     var title: String? = null,
-                     var message: String? = null,
-                     var positiveButton: Pair<String, DialogInterface.OnClickListener>? = null,
-                     var negativeButton: Pair<String, DialogInterface.OnClickListener>? = null): Parcelable
-
     class Factory(private val context: Context) {
-        private val model = Model()
-
         var customView: View? = null
 
-        var priority: Priority
-            get() = model.priority
-            set(value) { model.priority = value }
+        var priority: Priority = Priority.LOW
 
-        var isCancelable: Boolean
-            get() = model.isCancelable
-            set(value) { model.isCancelable = value }
+        var cancelable: Boolean = true
 
-        var title: String?
-            get() = model.title
-            set(value) { model.title = value }
-        fun setTitle(@StringRes resId: Int) { model.title = context.getString(resId) }
+        var title: String? = null
+        fun setTitle(@StringRes resId: Int) { title = context.getString(resId) }
 
-        var message: String?
-            get() = model.message
-            set(value) { model.message = value }
-        fun setMessage(@StringRes resId: Int) { model.message = context.getString(resId) }
+        var message: String? = null
+        fun setMessage(@StringRes resId: Int) { message = context.getString(resId) }
 
+        var positiveButton: Pair<String, DialogInterface.OnClickListener>? = null
         fun setPositiveButton(@StringRes resId: Int, listener: () -> Unit = {}) {
             setPositiveButton(context.getString(resId), listener)
         }
         fun setPositiveButton(message: String, listener: () -> Unit = {}) {
-            model.positiveButton = message to DialogInterface.OnClickListener { dialog, _ ->
+            positiveButton = message to DialogInterface.OnClickListener { dialog, _ ->
                 dialog.dismiss()
                 listener()
             }
         }
 
+        var negativeButton: Pair<String, DialogInterface.OnClickListener>? = null
         fun setNegativeButton(@StringRes message: Int, listener: () -> Unit = {}) {
             setNegativeButton(context.getString(message), listener)
         }
         fun setNegativeButton(message: String, listener: () -> Unit = {}) {
-            model.negativeButton = message to DialogInterface.OnClickListener { dialog, _ ->
+            negativeButton = message to DialogInterface.OnClickListener { dialog, _ ->
                 dialog.dismiss()
                 listener()
             }
         }
 
-        /**
-         * Create ApplicationDialog
-         */
         fun create() =
             ApplicationDialog().apply {
-                arguments = Bundle().apply {
-                    putParcelable(MODEL_KEY, model)
+                initializer {
+                    it.title = title
+                    it.message = message
+                    it.positiveButton = positiveButton
+                    it.negativeButton = negativeButton
+                    it.priority = priority
+                    it.isCancelable = cancelable
+                    it.customView = customView
                 }
-                cv = customView
             }
     }
 
@@ -89,42 +89,45 @@ class ApplicationDialog: DialogFragment() {
 
         private const val TAG = "application_dialog"
 
-        private const val MODEL_KEY = "application_dialog_model"
-
     }
 
     enum class Priority {
         HIGH, LOW
     }
 
-    private var priority: Priority = Priority.LOW
+    private val viewModel: ApplicationDialogModel by viewModels()
 
-    // TODO: passing custom view into fragment
-    private var cv: View? = null
+    /**
+     * Delay initializing until dialog is attached.
+     * Can call only ApplicationDialog.Factory.
+     */
+    private fun initializer(i: (ApplicationDialogModel) -> Unit) { initializer = i }
+    private var initializer: ((ApplicationDialogModel) -> Unit)? = null
 
     //------------------------------------------------------------------------------------------------------------------
     // MARK: Life cycle
     //------------------------------------------------------------------------------------------------------------------
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        initializer?.invoke(viewModel)
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
         AlertDialog.Builder(requireContext()).apply {
-            arguments?.getParcelable<Model>(MODEL_KEY)?.let {
-                setTitle(it.title)
-                setMessage(it.message)
+            setTitle(viewModel.title)
+            setMessage(viewModel.message)
 
-                it.positiveButton?.let { p ->
-                    setPositiveButton(p.first, p.second)
-                }
-                it.negativeButton?.let { n ->
-                    setNegativeButton(n.first, n.second)
-                }
-
-                isCancelable = it.isCancelable
-
-                priority = it.priority
-
-                cv?.let { v -> setView(v) }
+            viewModel.positiveButton?.let { p ->
+                setPositiveButton(p.first, p.second)
             }
+            viewModel.negativeButton?.let { n ->
+                setNegativeButton(n.first, n.second)
+            }
+
+            isCancelable = viewModel.isCancelable
+
+            viewModel.customView?.let { v -> setView(v) }
         }
             .create()
 
@@ -133,7 +136,7 @@ class ApplicationDialog: DialogFragment() {
     override fun show(manager: FragmentManager, tag: String?) {
         val presented = manager.findFragmentByTag(tag)
         if (presented is DialogFragment && presented.dialog?.isShowing == true) {
-            when (priority) {
+            when (viewModel.priority) {
                 // Overwrite dialog.
                 Priority.HIGH -> presented.dialog?.dismiss()
                 // Do not show
