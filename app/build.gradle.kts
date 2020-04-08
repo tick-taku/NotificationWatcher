1599064285f0a274cb28813844b7fd0c21145360
tick.taku.77 <tick.taku.77@gmail.com>
2020-04-08 17:55:22 +0900

    Fix compileOptions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,11 +56,10 @@ android {
             freeCompilerArgs += listOf("-Xuse-experimental=kotlin.Experimental")
         }
     }
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 dependencies {
