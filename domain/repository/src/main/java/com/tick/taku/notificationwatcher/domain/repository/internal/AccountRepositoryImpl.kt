b1f78614482b0da5c3cfc44a71230ff71a98404c
tick.taku.77 <tick.taku.77@gmail.com>
2020-05-12 15:21:25 +0900

    Use ConflatedBroadcastChannel instead of Channel.CONFLATE.

--- domain/repository/src/main/java/com/tick/taku/notificationwatcher/domain/repository/internal/AccountRepositoryImpl.kt
+++ domain/repository/src/main/java/com/tick/taku/notificationwatcher/domain/repository/internal/AccountRepositoryImpl.kt
@@ -5,26 +5,29 @@ import com.tick.taku.notificationwatcher.domain.api.account.AccountClient
 import com.tick.taku.notificationwatcher.domain.api.entity.AccountEntity
 import com.tick.taku.notificationwatcher.domain.repository.AccountRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 internal class AccountRepositoryImpl @Inject constructor(private val client: AccountClient): AccountRepository {
 
-    private val accountInfo: Channel<AccountEntity> = Channel(Channel.CONFLATED)
+    @UseExperimental(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    private val accountInfo: ConflatedBroadcastChannel<AccountEntity> = ConflatedBroadcastChannel()
 
     @UseExperimental(kotlinx.coroutines.FlowPreview::class, kotlinx.coroutines.ExperimentalCoroutinesApi::class)
     override fun accountInfo(): Flow<AccountEntity> =
-        accountInfo.consumeAsFlow()
+        accountInfo.asFlow()
             .onStart { getProfile() }
 
+    @UseExperimental(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
     private suspend fun getProfile() {
         withContext(Dispatchers.IO) {
             accountInfo.offer(client.obtainProfile())
         }
     }
 
+    @UseExperimental(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
     override suspend fun onAccountLinkResult(data: Intent?) {
         accountInfo.offer(client.onAccountLinked(data))
     }
