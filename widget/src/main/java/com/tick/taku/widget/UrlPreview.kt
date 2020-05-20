bbbac56ca0740e45e7b6f226be677a692372d0c0
tick.taku.77 <tick.taku.77@gmail.com>
2020-05-20 15:37:02 +0900

    Cut out Jsoup connection from View.

--- widget/src/main/java/com/tick/taku/widget/UrlPreview.kt
+++ widget/src/main/java/com/tick/taku/widget/UrlPreview.kt
@@ -3,7 +3,6 @@ package com.tick.taku.widget
 import android.content.Context
 import android.content.Intent
 import android.util.AttributeSet
-import android.util.LruCache
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import androidx.annotation.MainThread
@@ -15,25 +14,23 @@ import coil.transform.RoundedCornersTransformation
 import com.tick.taku.android.corecomponent.ktx.guard
 import com.tick.taku.android.corecomponent.ktx.isWebUrlSchema
 import com.tick.taku.widget.databinding.LayoutUrlPreviewBinding
+import com.tick.taku.widget.internal.ContentCrawler
 import com.tick.taku.widget.internal.SourceContent
-import com.tick.taku.widget.internal.mapper.toSourceContent
 import kotlinx.coroutines.*
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import timber.log.Timber
 import java.lang.IllegalArgumentException
 import java.lang.NullPointerException
 import kotlin.coroutines.CoroutineContext
-
-private val previewCache = LruCache<String, SourceContent>(1024 * 1024)
 
 class UrlPreview: FrameLayout, CoroutineScope {
     constructor(context: Context): super(context)
     constructor(context: Context, attrs: AttributeSet?): super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyle: Int): super(context, attrs, defStyle)
 
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main
+    override val coroutineContext: CoroutineContext = Dispatchers.Main
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        onRenderedError?.invoke(throwable)
+    }
+    private val coroutineScope = CoroutineScope(coroutineContext + exceptionHandler)
 
     var url: String = ""
         set(value) {
@@ -41,7 +38,7 @@ class UrlPreview: FrameLayout, CoroutineScope {
                 onRenderedError?.invoke(IllegalArgumentException("Argument is not web url."))
                 return
             }
-            showPreview(field)
+            makePreview(field)
         }
 
     var title: String = ""
@@ -66,41 +63,19 @@ class UrlPreview: FrameLayout, CoroutineScope {
     private val binding: LayoutUrlPreviewBinding by lazy {
         DataBindingUtil.inflate<LayoutUrlPreviewBinding>(LayoutInflater.from(context), R.layout.layout_url_preview, this, true)
     }
-    private var connection: Deferred<Document?>? = null
+    private var connection: Deferred<SourceContent?>? = null
 
     private val previewImageRadius: Float by lazy {
         context.resources.getDimension(R.dimen.url_preview_image_radius)
     }
 
-    private fun showPreview(webUrl: String) {
-        previewCache.get(webUrl).takeIf { it?.title?.isNotEmpty() == true }?.let { renderPreview(it) }
-            ?: obtainUrlPreview(webUrl)
-    }
-
-    private fun obtainUrlPreview(webUrl: String) {
-        launch {
-            connectAsync(webUrl).also { connection = it }.await()?.toSourceContent()?.let {
-                withContext(Dispatchers.Main) {
-                    renderPreview(it)
-                }
-                previewCache.put(webUrl, it)
+    private fun makePreview(webUrl: String) {
+        coroutineScope.launch {
+            ContentCrawler.obtainUrlPreviewAsync(webUrl).also { connection = it }.await()?.let {
+                renderPreview(it)
+                connection = null
             }
-            connection = null
         }
-    }
-
-    private fun connectAsync(webUrl: String) = async(Dispatchers.IO) {
-        Timber.d("Try to connect $webUrl.")
-        runCatching {
-            Jsoup.connect(webUrl).userAgent("Mozilla").get()
-        }
-            .onFailure {
-                Timber.e("Failed to connect $webUrl. <$it>")
-                withContext(Dispatchers.Main) {
-                    onRenderedError?.invoke(it)
-                }
-            }
-            .getOrNull()
     }
 
     @MainThread
@@ -127,7 +102,7 @@ class UrlPreview: FrameLayout, CoroutineScope {
         super.onAttachedToWindow()
 
         if (title.isEmpty())
-            showPreview(url)
+            makePreview(url)
     }
 
     override fun onDetachedFromWindow() {
